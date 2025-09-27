e8a228d5d4c242b3c3a3739a2f4b92ca9bf8c14e
jsparrowio <jsparrowio@outlook.com>
2025-09-26 22:43:45 -0700

    reached layout MVP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import './App.css'
+import { Layout, Menu, Breadcrumb } from 'antd';
+import fhsicon from "./assets/fhsicon.png";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout className="layout" style={{ minHeight: '100vh' }}>
+        <Header>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <div className="logo"><img src={fhsicon} style={{ height: "50px", width: "50px", marginRight: "15px", marginLeft: "-30px" }}></img></div>
+            <Menu.Item key="1">Home</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ textAlign: 'center' }}>
+          <div className="App">
+            <div className="landing-container">
+              <img src={fhsicon} style={{ 'height': '150px', 'width': '150px' }} alt="fierrettspherelogo"></img>
+              <h1 className="title">The Fierrett Sphere</h1>
+              <div className="message-container">
+                Welcome to the Fierrett Sphere landing page! This site is currently under construction, but should be up soon!
+              </div>
+            </div>
+          </div>
+        </Content>
+        <Footer style={{ background: "black", color: "white", textAlign: 'center', padding: " 0 0 1rem 0", paddingBottom: "1rem" }}>Fierrett Sphere © jsparrowio</Footer>
+      </Layout>
     </>
   )
 }

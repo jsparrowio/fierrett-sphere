9be2d5fa2fe8a45661cd68d99ec136abf8925115
jsparrowio <jsparrowio@outlook.com>
2025-09-29 22:16:51 -0700

    minor CLI and ENV changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ import db from './config/connection.js';
 import type { RequestHandler } from 'express';
 
 import { fileURLToPath } from 'node:url';
-import { createProxyMiddleware } from 'http-proxy-middleware'; 
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -34,14 +34,16 @@ const startApolloServer = async () => {
   );
 
   if (process.env.NODE_ENV === 'production') {
+    console.log('\x1b[1mYour app is running in production mode!');
     const address = path.join(__dirname, '../../client/dist');
     app.use(express.static(address));
 
     // app.get('*', (_req, res) => {
-    //   res.sendFile(path.join(address, 'index.html'));
+    //   res.sendFile(path.join(address, '/index.html'));
     // });
-  } else {
+  } else if (process.env.NODE_ENV === 'development') {
     // Proxy non-GraphQL requests to Vite dev server
+    console.log('\x1b[1mDev mode detected! \x1b[0m \nSetting up Vite proxy...');
     app.use(
       '/',
       createProxyMiddleware({
@@ -49,14 +51,27 @@ const startApolloServer = async () => {
         changeOrigin: true,
       })
     );
-    console.log('Proxying frontend to Vite dev server at http://localhost:5173');
+  } else {
+    console.log('\x1b[1mNo node env set... assuming dev mode... (did you set the node_env .env variable?) \x1b[0m \nSetting up Vite proxy...');
+    app.use(
+      '/',
+      createProxyMiddleware({
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+      })
+    );
   }
 
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  db.on('error', console.error.bind(console, '\x1b[31mMongoDB connection error:'));
 
   app.listen(PORT, () => {
-    console.log(`Apollo + Express running on port \x1b[1m${PORT}`);
-    console.log('\x1b[1m\x1b[32mYour service is now live! ✅\x1b[0m');
+    console.log(`\x1b[1mApollo + Express running on port \x1b[4m${PORT}`);
+    if (process.env.NODE_ENV === 'production') {
+      console.log('\x1b[1m\x1b[32mYour service is now live! ✅\x1b[0m');
+    }
+    else {
+      console.log('\x1b[1m\x1b[33mYour service now running in development mode 🚧\x1b[0m');
+    }
   });
 };
 
